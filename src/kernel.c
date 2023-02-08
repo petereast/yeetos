8db04260d32d7ce91585a8ff6ae6a4ba5bb507c5
Peter East <me@peterea.st>
2023-02-08 09:20:58 +0000

    Clear the terminal at startup, and clean up a bit

--- src/kernel.c
+++ src/kernel.c
@@ -1,13 +1,40 @@
 #include "kernel.h"
 
-// Start writing text mode to 0xb8000
+#include <stddef.h>
+#include <stdint.h>
+
+uint16_t terminal_make_char(char c, char colour) {
+	return (colour << 8) | c;
+}
+
+uint16_t* video_mem = (uint16_t*) VGA_TEXT_MODE_ADDR;
+
+void terminal_initialise() {
+	for (int h = 0; h < VGA_HEIGHT; h ++) {
+		for (int w = 0; w < VGA_WIDTH; w ++) {
+			video_mem[h * VGA_WIDTH + w] = terminal_make_char(' ', 0x30);
+		}
+	}
+
+}
+
+size_t strlen(const char* str)  {
+	size_t len = 0;
+	while(str[len]) {
+		len ++;
+	}
+
+	return len;
+}
+
+// Start writing text mode to 0xb7fff
 int currentLine = 0;
 void println(char* input) {
 	// Loop over the string, copying each char to the text mode buffer until we see a null char
 	int idx = 0;
 
 	// Each char is 2 bytes
-	int currentLineStart = 0xB8000 + (2* 80 * currentLine++);
+	int currentLineStart = 0xB8000 + (2 * VGA_WIDTH * currentLine++);
 
 	char current;
 	while ((current = input[idx]) != 0) {
@@ -23,9 +50,11 @@ void println(char* input) {
 }
 
 void kernel_main() {
+	terminal_initialise();
 	// If we write to 0xB8000, we can write text to the screen, let's give this a go!
 	println("YeetOS 0.1 - github.com/petereast/yeetos");
 	println("---");
 	println("Hello world!");
 	println("This is a new line");
+	println("this is a really lon string that will be longer than 80 chars in the strings yooooooooooooooooooooooo");
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,6 +1,10 @@
 #ifndef kernel_h
 #define kernel_h
 
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 24
+#define VGA_TEXT_MODE_ADDR 0xB8000
+
 void kernel_main();
 
 #endif
